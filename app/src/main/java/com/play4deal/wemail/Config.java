9fe7e2e21a909032791e27dbb0f34e24a3585da3
venugopalachary <venugopalachary7396@gmail.com>
2019-06-11 12:38:03 +0530

    Initial commit

---
+++ app/src/main/java/com/play4deal/wemail/Config.java
@@ -0,0 +1,8 @@
+package com.play4deal.wemail;
+
+public class Config {
+
+    public static String EMAIL ="abc739691@gmail.com";  //gmail address
+    public static String PASSWORD ="7396917874"; //password
+
+}
